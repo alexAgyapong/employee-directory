b48b38d2c7b6f2cd75c520c8af55324a39bfb448
Alexander Agyapong <30531859+alexAgyapong@users.noreply.github.com>
2020-12-30 15:55:20 +0000

    Add employee service and employee list ( Initial)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: EmployeeListComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
