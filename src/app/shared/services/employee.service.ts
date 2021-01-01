304fb51a97a853bdf26917e81f205db3b922d013
Alexander Agyapong <30531859+alexAgyapong@users.noreply.github.com>
2021-01-01 17:47:19 +0000

    Add error handling

--- src/app/shared/services/employee.service.ts
+++ src/app/shared/services/employee.service.ts
@@ -1,9 +1,9 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, pipe } from 'rxjs';
+import { Observable, pipe, throwError } from 'rxjs';
 import { Department } from '../models/department';
 import { Employee } from '../models/employee';
-import { tap } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 
 
 @Injectable({
@@ -11,49 +11,64 @@ import { tap } from 'rxjs/operators';
 })
 export class EmployeeService {
   private readonly baseURL = 'http://localhost:3000';
+
   constructor(private http: HttpClient) { }
 
   getEmployees(): Observable<Employee[]> {
-    const url = `${this.baseURL}/employees`;
-    return this.http.get<any>(url);
+    const url = `${this.baseURL}/employeess`;
+    return this.http.get<any>(url).pipe(catchError(this.handleError));
   }
 
   searchEmployees(searchTerm: string): Observable<Employee[]> {
     const url = `${this.baseURL}/employees?q=${searchTerm}`;
-    return this.http.get<any>(url);
+    return this.http.get<any>(url).pipe(catchError(this.handleError));
   }
 
   filterEmployeesByDepartment(id: number): Observable<Employee[]> {
     const url = `${this.baseURL}/employees?departmentId=${id}`;
-    return this.http.get<any>(url);
+    return this.http.get<any>(url).pipe(catchError(this.handleError));
   }
 
   getEmployeeDetail(id: number): Observable<Employee> {
     const url = `${this.baseURL}/employees/${id}`;
-    return this.http.get<any>(url);
+    return this.http.get<any>(url).pipe(catchError(this.handleError));
   }
 
   addEmployee(employee: Employee): Observable<Employee> {
     const url = `${this.baseURL}/employees`;
-    return this.http.post<any>(url, employee);
+    return this.http.post<any>(url, employee).pipe(catchError(this.handleError));
   }
 
   updateEmployeeDetail(employee: Employee): Observable<Employee> {
     const url = `${this.baseURL}/employees/${employee.id}`;
-    return this.http.put<any>(url, employee);
+    return this.http.put<any>(url, employee).pipe(catchError(this.handleError));
   }
 
   deleteEmployeeDetail(id: number): Observable<Employee> {
     const url = `${this.baseURL}/employees/${id}`;
-    return this.http.delete<any>(url);
+    return this.http.delete<any>(url).pipe(catchError(this.handleError));
   }
 
   getDepartments(): Observable<Department[]> {
     const url = `${this.baseURL}/departments`;
     return this.http.get<any>(url)
       .pipe(
-        tap(data => localStorage.setItem('departments', JSON.stringify(data)))
+        tap(data => localStorage.setItem('departments', JSON.stringify(data))),
+        catchError(this.handleError)
       );
+  }
+
+  handleError(error: HttpErrorResponse): Observable<any> {
+    let errorMessage = '';
+
+    if (error instanceof ErrorEvent) {
+      errorMessage = `Error: ${error}`;
+    } else {
+      errorMessage = `Error Code: ${error?.status}\nMessage: ${error?.statusText}`;
+      window.alert(errorMessage);
+    }
+
+    return throwError(error);
   }
 
 
